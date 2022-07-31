ae76aca8e6cb133a52ce77fb55224e5bd3923f47
Carlos01111 <carlosforero2023@gmail.com>
2022-07-30 20:38:26 -0500

    views:home,detailpost

--- bases/views.py
+++ bases/views.py
@@ -1,9 +1,10 @@
+from multiprocessing import context
 from django.shortcuts import render
 #vistas genericas
 from django.views.generic import ListView, DetailView
 
 #models
-from bases.models import Post
+from bases.models import Post, PostContent
 # Create your views here.
 
 class HomeView(ListView):
@@ -18,3 +19,9 @@ class DetailPostView(DetailView):
     template_name = 'detailpost.html'
     model = Post
     context_object_name = 'posts'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        contents = PostContent.objects.all()
+        context['contents']= contents
+        return context
